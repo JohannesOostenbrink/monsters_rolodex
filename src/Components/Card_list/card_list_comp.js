1c841dab80fd3ba99bfdfdc701f245d0fa050242
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-23 11:00:53 +0700

    Small error fix

--- src/Components/Card_list/card_list_comp.js
+++ src/Components/Card_list/card_list_comp.js
@@ -9,16 +9,16 @@ class CardList extends Component{
         return(
             <div className = 'card-list'>
                     {monsters.map(monster => {
-                        const {Name , email ,id}  = monster;
+                        const {name , email ,id}  = monster;
 
                     
                     
                     return(
                         <div className = 'card-container' key={id}>
                            <img  
-                            alt={`monster ${Name}`}
+                            alt={`monster ${name}`}
                             src= {`https://robohash.org/${id}?set=set2&size=180x180`}/>
-                            <h2>{Name}</h2>
+                            <h2>{name}</h2>
                             <p>{email}</p>
 
                         </div>
